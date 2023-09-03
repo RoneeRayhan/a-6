843baefc71c201b90c640bd7f7df542865811907
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-09-03 21:47:14 +0600

    Initial commit

---
+++ express-project/index.js
@@ -0,0 +1,41 @@
+/* const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+// MongoDB Connection
+mongoose
+  .connect('mongodb://localhost/your-database-name', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.error('MongoDB Connection Error:', err));
+
+// API Routes
+app.use('/api', require('./src/Routes/api')); // Replace with your actual API routes //routes //lowercase
+
+// Serve static assets if in production (e.g., React app)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+ */
